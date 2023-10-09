d74f22775078ea6cc6abfbec57ad155acf3f89e8
carlos <carlos@3pace.dev>
2023-10-08 21:00:59 -0300

    further stylins

--- app/products/ctasButtons.tsx
+++ app/products/ctasButtons.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const CtasButtons = () => {
   return (
-    <div className="flex justify-center">
-      <button className="m-8 w-auto text-black bg-white hover:bg-btn-background-hover rounded-full text-xl px-4 py-2 mb-2 overflow-hidden whitespace-nowrap">
-        TOTAL RESIDUOS 2023
+    <div className="flex flex-col md:flex-row justify-center mb-16">
+      <button className="m-8 w-auto text-black bg-white hover:bg-btn-background-hover hover:text-white rounded-full text-xl px-4 py-2 mb-2 overflow-hidden whitespace-nowrap">
+        <strong>TOTAL RESIDUOS 2023</strong>
       </button>
-      <button className="m-8 w-auto text-black bg-white hover:bg-btn-background-hover rounded-full text-xl px-4 py-2 mb-2 overflow-hidden whitespace-nowrap">
-        TOTAL RESIDUOS 2024
+      <button className="m-8 w-auto text-black bg-white hover:bg-btn-background-hover hover:text-white rounded-full text-xl px-4 py-2 mb-2 overflow-hidden whitespace-nowrap">
+        <strong>TOTAL RESIDUOS 2024</strong>
       </button>
     </div>
   )
