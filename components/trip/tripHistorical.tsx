56e2f17332f8c434732dc76ebf0b977425213aff
carlos <carlos@3pace.dev>
2023-10-13 11:12:07 -0300

    line and dot positining

--- components/trip/tripHistorical.tsx
+++ components/trip/tripHistorical.tsx
@@ -3,19 +3,20 @@ import JourneyLine from 'assets/images/icons/SVG/journey/journey-line.svg'
 import JourneyDot from 'assets/images/icons/SVG/journey/journey-dot.svg'
 const TripHistorical = () => {
   return (
-    <div className="z-0 w-full flex border rounded-xl  bg-[#79d97c] shadow shadow-xs shadow-gray-300  h-64">
-      <div className="relative h-full w-1/2 "></div>
-      <div className="relative h-full w-1/2 ">
-        <div className="absolute inset-0 md:inset-0 z-99999">
+    <div className="mb-20 w-full flex border rounded-xl  bg-[#79d97c] shadow shadow-xs shadow-gray-300  h-64">
+      <div className="relative h-full w-1/2 ">textos</div>
+      <div className="relative h-full w-1/2">
+        <div className="absolute  right-0 top-0">
           <Image
             src={JourneyLine}
             alt="theCirculArt Journey"
-            layout="fill"
-            objectFit="contain"
+            // fill={true}
+            objectFit="cover"
             objectPosition="right"
+            style={{ width: '100%', height: '120%' }}
           />
         </div>
-        <div className="absolute right-5 md:right-20 top-16 md:top-20 z-1">
+        <div className="absolute right-20 top-20">
           <Image
             src={JourneyDot}
             height={65}
