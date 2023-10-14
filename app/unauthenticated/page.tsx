82a018ae73f7ab6f6fa52dffd4d1e9363d2c8713
carlos <carlos@3pace.dev>
2023-10-14 10:45:44 -0300

    added force dynamic to avoid build error

--- app/unauthenticated/page.tsx
+++ app/unauthenticated/page.tsx
@@ -3,6 +3,8 @@ import { cookies } from 'next/headers'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Unauthenticated() {
   const supabase = createServerComponentClient({ cookies })
   const {
