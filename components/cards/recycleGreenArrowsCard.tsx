fc65b2e7daf7e80ea1802fe583413f6f3e5e66e8
carlos <c.delriofrances@gmail.com>
2023-11-24 19:01:21 -0300

    to be continued

--- components/cards/recycleGreenArrowsCard.tsx
+++ components/cards/recycleGreenArrowsCard.tsx
@@ -4,17 +4,25 @@ import { ReactNode } from 'react' // Import ReactNode
 
 const RecycleGreenArrowsCard = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="z-0 relative w-full h-44 md:h-64 border rounded-xl bg-none dark:bg-gray-300 dark:shadow dark:shadow-md dark:shadow-gray-500">
-      <Image
-        src={RecycleGreenArrows}
-        alt="TheCirculArt Green Recycle Arrows"
-        layout="fill"
-        objectFit="contain"
-        objectPosition="left"
-        style={{ zIndex: -1 }}
-        className="ml-4 md:ml-10"
-      />
-      {children}
+    <div>
+      {/* first hover effect */}
+      <p className="m-16 relative group">
+        {' '}
+        <span>Hover over me</span>{' '}
+        <span className="absolute -bottom-1 left-0 w-0 h-2 bg-blue-400 transition-all group-hover:w-full"></span>
+      </p>
+      <div className="z-0 relative w-full h-44 md:h-64 border rounded-xl bg-none dark:bg-gray-300 dark:shadow dark:shadow-md dark:shadow-gray-500">
+        <Image
+          src={RecycleGreenArrows}
+          alt="TheCirculArt Green Recycle Arrows"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+          style={{ zIndex: -1 }}
+          className="ml-4 md:ml-10"
+        />
+        {children}
+      </div>
     </div>
   )
 }
