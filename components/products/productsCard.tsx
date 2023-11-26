47c8042236c49621f0dc128c13952100e0201f8b
carlos <c.delriofrances@gmail.com>
2023-11-26 14:22:59 -0300

    cards hover finished

--- components/products/productsCard.tsx
+++ components/products/productsCard.tsx
@@ -14,9 +14,11 @@ const ProductsCard = ({ products }: { products: ProductProps }) => {
   return (
     <RecycleGreenArrowsCard>
       <div className="flex flex-col mr-6 md:mr-20 items-end mt-6 md:mt-20">
-        <div className="flex flex-col mr-2 items-end align-center">
-          <h1 className="z-1 text-gray-600">{products?.length} Productos</h1>
-          <h3 className="z-1 text-gray-600 -mt-5">reciclados</h3>
+        <div className="flex flex-col mr-2">
+          <h1 className="z-1 text-gray-600 leading-8">
+            {products?.length} Categorías de productos
+          </h1>
+          <h3 className="z-1 text-gray-600">reciclados</h3>
         </div>
         {/* <h5 className="text-gray-500">{productNames}</h5> */}
       </div>
