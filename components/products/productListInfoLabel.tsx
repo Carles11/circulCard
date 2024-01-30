4b9e44cd7f935d7f15e11af995c79331f68b956f
carlos <c.delriofrances@gmail.com>
2024-01-30 14:34:04 -0300

    fixed displaying values in dropdown

--- components/products/productListInfoLabel.tsx
+++ components/products/productListInfoLabel.tsx
@@ -33,21 +33,25 @@ const ProductListInfoLabel = ({
     <div className="w-full text-left">
       <ul className="text-gray-700">
         <li>
-          <h5>Unidades gestionadas:</h5>
-          <div className="flex gap-2 items-baseline">
-            <h2> {units[0].unidades_gestionadas_total || 0} </h2>{' '}
-            <p>{productName}</p>
+          <h5>Hasta hoy hemos gestionado </h5>
+          <div className="flex gap-2 items-center">
+            <h2 className="text-5xl">
+              {' '}
+              {units[0].unidades_gestionadas_total || 0}{' '}
+            </h2>{' '}
+            <p>unidades de {productName} para ti.</p>
           </div>
         </li>
+        <hr className="w-full h-px bg-gray-400 border-0 rounded md:my-2 dark:bg-gray-500" />
         <li>
-          <h5>Hemos recogido</h5>
-          <div className="flex gap-2 items-center">
-            <h2>
+          <div className="flex gap-1 items-center">
+            <h5>En total hemos recogido</h5>
+            <h2 className="text-6xl">
               {units[0].peso_total ? convertedWeight : 0}
               {weightUnit}
             </h2>
-            <p>de este producto en tu empresa.</p>
           </div>
+          <p>de este producto en tu empresa.</p>
         </li>
         {productName === 'tarjetas' &&
           certificates &&
